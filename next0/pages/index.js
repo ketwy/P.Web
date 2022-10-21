525990298863581116757fa644d2b86adefcb5e6
ketwy <ketllyazevedo14@gmail.com>
2022-10-21 13:22:28 -0300

    Receita 1: questão 1

---
+++ next0/pages/index.js
@@ -0,0 +1,19 @@
+export default function Home(){
+    return (
+        <div>
+            <h2>
+                Viva Santana!
+            </h2>
+        </div>
+    )
+    }
+
+export function Home2(){
+        return (
+            <div>
+                <h2>
+                    Funcao de Teste
+                </h2>
+            </div>
+        )
+        }
---
+++ next0/pages/questao1.js
@@ -0,0 +1,14 @@
+export default function QuestaoUm() {
+    return (
+        <div>
+            <table border="1">
+                <thead>
+                    <tr><th>Nome</th><th>Marca</th></tr>
+                </thead>            
+                <tbody>
+                    <tr><td>Chinelos</td><td>Havanhanas</td></tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
